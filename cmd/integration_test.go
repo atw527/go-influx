bfa95547a6611e4ae57f149dcf845ecb6e3b0217
Andrew Wells <andrew@wellsie.net>
2018-02-11 18:16:50 -0800

    Update tests

--- cmd/integration_test.go
+++ cmd/integration_test.go
@@ -3,14 +3,14 @@
 package main
 
 import (
-    "os"
-    "testing"
+	"os"
+	"testing"
 )
 
 func TestWritePoints(t *testing.T) {
-    os.Setenv("INFLUX_HOST", "localhost")
-    os.Setenv("INFLUX_PORT", "8086")
-    os.Setenv("INFLUX_DB", "testing")
+	os.Setenv("INFLUX_HOST", "localhost")
+	os.Setenv("INFLUX_PORT", "8086")
+	os.Setenv("INFLUX_DB", "testing")
 
-    writePoints(10, 32, 32, 0, "pulse")
+	writePoints(10, 32, 32, 0, "pulse")
 }
--- goinflux/integration_test.go
+++ goinflux/integration_test.go
@@ -3,33 +3,33 @@
 package goinflux
 
 import (
-    "os"
-    "testing"
-    "time"
+	"os"
+	"testing"
+	"time"
 )
 
 func TestInflux(t *testing.T) {
-    os.Setenv("INFLUX_HOST", "localhost")
-    os.Setenv("INFLUX_PORT", "8086")
-    os.Setenv("INFLUX_DB", "testing")
+	os.Setenv("INFLUX_HOST", "localhost")
+	os.Setenv("INFLUX_PORT", "8086")
+	os.Setenv("INFLUX_DB", "testing")
 
-    tags := TagGroup{}
-    tags["metric"] = "pulse"
+	tags := TagGroup{}
+	tags["metric"] = "pulse"
 
-    fields := FieldGroup{}
-    fields["value"] = 1
+	fields := FieldGroup{}
+	fields["value"] = 1
 
-    gi, err := NewGoInflux(os.Getenv("INFLUX_HOST"), os.Getenv("INFLUX_PORT"), 25, 25, 5)
-    if err != nil {
-        t.Fatalf("Error init: %v\n", err.Error())
-        return
-    }
+	gi, err := NewGoInflux(os.Getenv("INFLUX_HOST"), os.Getenv("INFLUX_PORT"), 25, 25, 5)
+	if err != nil {
+		t.Fatalf("Error init: %v\n", err.Error())
+		return
+	}
 
-    err = gi.AddPoint("test", tags, fields, time.Now().UnixNano())
-    if err != nil {
-        t.Fatalf("Error point: %v\n", err.Error())
-        return
-    }
+	err = gi.AddPointError("test", tags, fields, time.Now().UnixNano())
+	if err != nil {
+		t.Fatalf("Error point: %v\n", err.Error())
+		return
+	}
 
-    gi.Stahp()
+	gi.Stahp()
 }
